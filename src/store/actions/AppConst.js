f25e28ebab110a9e36d9591c0bf2681ef529600e
JMcCloskey <jmccloskey@JMcCloskeys-MacBook-Pro.local>
2019-08-19 12:15:50 +0530

    Beta server setup done

--- src/store/actions/AppConst.js
+++ src/store/actions/AppConst.js
@@ -3,11 +3,9 @@
 
 //beta server
 // export const BASE_URL = '/beta/sales/sales_engine/api/'; 
-
 export const BASE_URL = '/sales_engine/api/'; 
 
 //local server
 // export const BASE_URL = 'http://localhost:8888/sales_engine/api/'
-
 export const COUNTRY = localStorage.getItem('currency');
 
